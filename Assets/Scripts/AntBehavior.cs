1ce8f32d39647a29d566aa9b29d30c5ff4ebc88b
chengzansong <lukesong2008@gmail.com>
2026-03-25 00:27:55 -0700

    before-big-pheromone-change
    temp fix for everything before big pheromone change

--- Assets/Scripts/AntBehavior.cs
+++ Assets/Scripts/AntBehavior.cs
@@ -15,17 +15,19 @@ public class AntBehavior : MonoBehaviour
     Vector2 position, velocity, desired_direction, desired_velocity, desired_force, acceleration;
     [SerializeField] bool selectedfood = false;
     public bool carryingfood = false;
-    List<GameObject> to_food_pheromone_in_range = new List<GameObject>();
-    List<GameObject> to_home_pheromone_in_range = new List<GameObject>();
+    List<GameObject> to_food_pheromone_in_range = new List<GameObject>(), to_home_pheromone_in_range = new List<GameObject>();
     AntBehavior ant;
     private bool foundbase = false;
     private readonly Guid id = Guid.NewGuid();
     public Guid Id => id;
     List<Collider2D> to_food_pheromone_colliders = new List<Collider2D>(), to_home_pheromone_colliders = new List<Collider2D>();
     public ContactFilter2D tohome, tofood;
-    [SerializeField] float check_pheromone = 1f, check_countdown = 1f;
+    [SerializeField] float check_pheromone = 1f;
+    private float check_countdown = 1f;
+    private Transform pheromone_placeholder;
     void Start()
     {
+        pheromone_placeholder = GameObject.Find("Spawned_pheromones").transform;
         check_countdown = UnityEngine.Random.Range(0f,1f);
         position = transform.position;
         velocity = new Vector2(0,0);
@@ -35,25 +37,32 @@ public class AntBehavior : MonoBehaviour
         check_countdown -= Time.deltaTime;
         if(check_countdown <= 0)
         {
-            Vector2 center = transform.position + transform.up*3f;
             check_countdown = check_pheromone;
-            to_food_pheromone_in_range.Clear();
-            Physics2D.OverlapCircle(center, 3, tofood, to_food_pheromone_colliders); 
-            for (int i = 0; i < to_food_pheromone_colliders.Count; i++)
-            {
-                if(to_food_pheromone_colliders[i].gameObject == null) continue;
-                to_food_pheromone_in_range.Add(to_food_pheromone_colliders[i].gameObject);
-            }
-            to_home_pheromone_colliders.Clear();
-            Physics2D.OverlapCircle(center, 3, tohome, to_home_pheromone_colliders);  
-            for (int i = 0; i < to_home_pheromone_colliders.Count; i++)
-            {
-                if(to_home_pheromone_colliders[i].gameObject == null) continue;
-                to_home_pheromone_in_range.Add(to_home_pheromone_colliders[i].gameObject);
-            }
+            checkfood();
         }
         directionDecision();
         movementPhysics();
+    }
+    void checkfood()
+    {
+        Vector2 center = transform.position + transform.up*3f;
+        to_food_pheromone_in_range.Clear();
+        to_food_pheromone_colliders.Clear();
+        float detection_displacement = 3f;
+        Physics2D.OverlapCircle(center, detection_displacement, tofood, to_food_pheromone_colliders); 
+        for (int i = 0; i < to_food_pheromone_colliders.Count; i++)
+        {
+            if(to_food_pheromone_colliders[i].gameObject == null) continue;
+            to_food_pheromone_in_range.Add(to_food_pheromone_colliders[i].gameObject);
+        }
+        to_home_pheromone_in_range.Clear();
+        to_home_pheromone_colliders.Clear();
+        Physics2D.OverlapCircle(center, detection_displacement, tohome, to_home_pheromone_colliders);  
+        for (int i = 0; i < to_home_pheromone_colliders.Count; i++)
+        {
+            if(to_home_pheromone_colliders[i].gameObject == null) continue;
+            to_home_pheromone_in_range.Add(to_home_pheromone_colliders[i].gameObject);
+        }
     }
     void movementPhysics()
     {
@@ -83,21 +92,33 @@ public class AntBehavior : MonoBehaviour
         }
         else if(!selectedfood && carryingfood)//foundfood, heading back to base
         {
-            Vector2 newdirection = UnityEngine.Random.insideUnitCircle*wanderstrength;
-            if(calculate_pheromones(to_home_pheromone_in_range) != new Vector2(0,0))
+            if(to_home_pheromone_in_range.Count > 0)
             {
-                desired_direction = calculate_pheromones(to_home_pheromone_in_range);
+                Vector2 calculated_direction = calculate_pheromones(to_home_pheromone_in_range);
+                desired_direction = calculated_direction; // this is either += or just =; i'm not sure which is better;
             }
+
+            Vector2 newdirection = UnityEngine.Random.insideUnitCircle*wanderstrength;
             desired_direction += newdirection * wanderstrength;
+
             Vector2 tohome = ((Vector2)anthill.transform.position - (Vector2)transform.position).normalized;
             desired_direction += tohome * home_attraction;
+
             desired_direction = desired_direction.normalized;
             lay_pheromones("tofood");
         }
         else //randomly wander, with pheromones
         {
+            if(to_food_pheromone_in_range.Count > 0)
+            {
+                Vector2 calculated_direction = calculate_pheromones(to_food_pheromone_in_range);
+                desired_direction = calculated_direction;
+            }
+            
             Vector2 newdirection = UnityEngine.Random.insideUnitCircle*wanderstrength;
-            desired_direction += (calculate_pheromones(to_food_pheromone_in_range) + newdirection * wanderstrength).normalized;
+            desired_direction += newdirection * wanderstrength;
+            desired_direction = desired_direction.normalized;
+
             lay_pheromones("tohome");
         }
     }
@@ -117,13 +138,9 @@ public class AntBehavior : MonoBehaviour
             else
             {
                 temppheromone = usedlist[i].GetComponent<PheromoneBehavior>();
-                //note: later fix, currently sqr magnitude is the magnitude squared, which is really fast, but may throw it off balance.
-                
-                // makes it so they always have a normalization factor wandering towards the pheromone; also makes it 
-                // so if the pheromones are farther away it just cancels out
                 Vector2 towards_pheromone = ((Vector2)temppheromone.transform.position - (Vector2)transform.position).normalized;
-                Vector2 normal = transform.right;
-                float angle = Vector2.Angle(towards_pheromone,normal);
+                Vector2 normal = transform.up;
+                float angle = Vector2.SignedAngle(normal,towards_pheromone);
                 totalangle += angle*temppheromone.decay;
                 totaldecay += temppheromone.decay;
             }
@@ -131,11 +148,11 @@ public class AntBehavior : MonoBehaviour
         if(totaldecay != 0 && totalangle !=0)
         {
             totalangle/=totaldecay;   
-            direction = new Vector2(Mathf.Cos(totalangle * Mathf.Deg2Rad), Mathf.Sin(totalangle * Mathf.Deg2Rad));
+            direction = new Vector2(Mathf.Sin(totalangle * Mathf.Deg2Rad), Mathf.Cos(totalangle * Mathf.Deg2Rad));
             direction = direction.normalized;
         }
-        Vector2 worldDir = transform.TransformDirection(direction);
-        return worldDir;
+        direction = Quaternion.Euler(0, 0, totalangle) * (Vector2)transform.up;
+        return direction;
     }
     void lay_pheromones(String type)
     {
@@ -143,10 +160,9 @@ public class AntBehavior : MonoBehaviour
         if(countdown <= 0)
         {
             countdown = time_between_pheromone;
-            GameObject gameObject = Instantiate(pheromone, transform.position, Quaternion.identity);
+            GameObject gameObject = Instantiate(pheromone, transform.position, Quaternion.identity, pheromone_placeholder);
             PheromoneBehavior newPheromone = gameObject.GetComponent<PheromoneBehavior>();
             newPheromone.initialize(type);//initialize it according to its type
-            newPheromone.direction = velocity*(-1);
         }
     }
     void OnTriggerEnter2D(Collider2D collision)
--- Assets/Scripts/PheromoneBehavior.cs
+++ Assets/Scripts/PheromoneBehavior.cs
@@ -5,15 +5,18 @@ using UnityEngine.Rendering;
 public class PheromoneBehavior : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public Vector2 direction;
     public float decay = 1f, evaporationtime = 1f;
-    private float lifetime = 0f;
+    private float lifetime = 0f, normalized_distance;
     SpriteRenderer sr;
     Color baseColor;
     [SerializeField] Color tohome, tofood;
+    public Transform top_wall, left_wall, anthole;
     void Awake()
     {
-        sr = GetComponentInChildren<SpriteRenderer>();
+        sr = GetComponentInChildren<SpriteRenderer>();  
+        top_wall = GameObject.Find("Wall_Top").transform;
+        left_wall = GameObject.Find("Wall_Left").transform;
+        anthole = GameObject.Find("Anthole").transform;
     }
 
     // Update is called once per frame
@@ -31,12 +34,19 @@ public class PheromoneBehavior : MonoBehaviour
     }
     public void initialize(String type)
     {
+        Vector2 corner = new Vector2(top_wall.position.y, left_wall.position.x);
+        float max_distance = Vector2.Distance(anthole.position, corner);
+        float distance_from_anthole = Vector2.Distance(transform.position, anthole.position);
+        normalized_distance = Mathf.Clamp01(distance_from_anthole / max_distance);
+        //Debug.Log($"normalized distance {normalized_distance}");
         if(type == "tofood")
         {
             baseColor = tofood;
             gameObject.tag = "tofood_pheromone";
             sr.color = baseColor;
             gameObject.layer = LayerMask.NameToLayer("tofood_pheromone");
+            evaporationtime *= Mathf.Lerp(0.1f, 1.8f, normalized_distance);
+            
         }
         else if(type == "tohome")
         {
@@ -44,6 +54,7 @@ public class PheromoneBehavior : MonoBehaviour
             gameObject.tag = "tohome_pheromone";
             sr.color = baseColor;
             gameObject.layer = LayerMask.NameToLayer("tohome_pheromone");
+            evaporationtime *= Mathf.Lerp(1.8f, 0.1f, normalized_distance);
         }
     }
 }
